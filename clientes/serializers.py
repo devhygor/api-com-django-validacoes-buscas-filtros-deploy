9626d022688d87be4813fc09236294555c3f84ed
Hygor Rocha <hygor.k92@gmail.com>
2024-04-28 14:52:08 -0300

    adcionando validaçoes no serializar para o cliente

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -6,3 +6,22 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
     
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError('O CPF deve ter 11 dígitos')
+        return cpf
+
+    def validade_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('Este campo nao pode ter números e nem caracteres especiais')
+        return nome
+
+    def validade_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError('O RG deve ter 9 dígitos')
+        return rg
+
+    def validade_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError('O número do celular deve ter pelo menos 11 dígitos')
+        return celular
